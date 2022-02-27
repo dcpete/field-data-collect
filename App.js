b6b0d8e5a9d56efd5ec36bfb6c37b92ec0cbeb40
Donny Peterson <peterson.donaldc@gmail.com>
2022-02-26 20:29:36 -0600

    Added Drawer navigation

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
+import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import RootReducer from './src/reducers'
 import HomeContainer from './src/screens/Home/HomeContainer';
 import Other from './src/screens/Other/Other';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
-const store = createStore(RootReducer);
+const Drawer = createDrawerNavigator()
+
+const store = createStore(RootReducer)
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator
+        <Drawer.Navigator
         screenOptions={{
           headerStyle: {
             backgroundColor: '#f4541f',
@@ -26,9 +30,9 @@ const App = () => {
           },
         }}
         initialRouteName={"Home"} >
-          <Stack.Screen name="Home" component={HomeContainer} options={{ title: 'My Home' }} />
-          <Stack.Screen name="Other" component={Other} options={{ title: 'Other Screen' }} />
-        </Stack.Navigator>
+          <Drawer.Screen name="Home" component={HomeContainer} options={{ title: 'My Home' }} />
+          <Drawer.Screen name="Other" component={Other} options={{ title: 'Other Screen' }} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </Provider>
   )
